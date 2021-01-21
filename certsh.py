4d724840caf353954018cb460d853fe9f2bf2f0a
Jonathan Toledo <overjt@users.noreply.github.com>
2021-01-21 13:53:58 -0500

    Update certsh.py

--- certsh.py
+++ certsh.py
@@ -19,9 +19,9 @@ class crtShClass():
 		for row in tableRows:
 			try:
 				subdomain = row.find_all('td')[4].text
-                                subdomain = subdomain.replace("*.","")
-                                if subdomain not in self.foundURLsList:
-				    self.foundURLsList.append(subdomain)
+				subdomain = subdomain.replace("*.","")
+				if subdomain not in self.foundURLsList:
+					self.foundURLsList.append(subdomain)
 			except Exception as e:
 				pass
 
